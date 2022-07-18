2c47e0aa54dc32190714828696f05e37fddd1aa7
Maarten Joosten <mjoosten@student.codam.nl>
2022-07-18 13:10:00 +0200

    added gitignore

--- include/vector.hpp
+++ include/vector.hpp
@@ -1,8 +1,6 @@
 #ifndef VECTOR_HPP
 #define VECTOR_HPP
 
-// https://github.com/electronicarts/EASTL/blob/master/include/EASTL/vector.h
-
 #include <memory> // std::allocator
 
 namespace ft 
@@ -10,43 +8,123 @@ namespace ft
 
 	template <typename T, class Allocator = std::allocator<T> >
 	class vector {
+
 		public:
-			typedef T*	iterator;
+		
+			//	Typedefs
+		
+			typedef T				value_type;
+			typedef Allocator		allocator_type;
+			typedef	std::size_t		size_type;
+			typedef	std::ptrdiff_t	difference_type;
+			typedef	T&				reference;
+			typedef	const T&		const_reference;
+			typedef	T*				pointer;
+			typedef	const T*		const_pointer;
+			typedef T*				iterator;
+			typedef	const T*		const_iterator;
+			typedef	T*				reverse_iterator; // TODO
+			typedef	const T*		const_reverse_iterator; // TODO
+
+			//	General member functions
 		
 			vector(): _begin(NULL), _end(NULL), _capacity(0) {}
-			//vector( const vector& rhs );
+			vector( const vector& rhs ) { *this = rhs; }
 			~vector() {
-				delete _begin;
+				_allocator.deallocate(_begin, _capacity);
 			};
 			//vector&	operator=( const vector& rhs );
+			Allocator	get_allocator() const {
+				return _allocator;
+			}
+
+			//	Element access
+		
+			reference	at(size_type index) {
+				if (index >= size())
+					throw std::out_of_range("vector");
+				return _begin[index];
+			}
+		
+			reference	operator[](size_type index) {
+				return _begin[index];
+			}
+					
+			reference	front() {
+				return *_begin;
+			}
+		
+			reference	back() {
+				return *(_end - 1);
+			}
+		
+			//	Iterators
 		
 			iterator	begin() const {
 				return _begin;
 			}
+
 			iterator	end() const {
 				return _end;
 			}
-			size_t	capacity() const {
-				return _capacity;
+
+			//	Capacity
+	
+			bool	empty() const {
+				return size() == 0;
 			}
-			size_t	size() const {
+
+			size_type	size() const {
 				return _end - _begin;
 			}
 
+			size_type	max_size() const {
+				return _allocator.max_size();
+			}
+
+			void	reserve(size_type n) {
+				if (n > _capacity)
+					setCapacity(n);
+			}
+
+			size_type	capacity() const {
+				return _capacity;
+			}
+
+			//	Modifiers
+
 			void	push_back(const T& value) {
-				if (size() == _capacity) {
-					_capacity = _capacity ? _capacity * 2 : 1;
-					_begin = Allocator::allocate(_capacity);
-				}
+				if (size() == _capacity)
+					setCapacity(_capacity ? _capacity * 2 : 1);
 				*_end = value;
 				_end++;
 			}
-			
+			void	pop_back() {
+				_end--;
+			}
+
+
 		protected:
-			T*		_begin;
-			T*		_end;
-			size_t	_capacity;
-	};
+	
+			void	setCapacity(size_type newCapacity) {
+				T*			tmp = _allocator.allocate(newCapacity);
+				size_type	newSize = size();
+			
+				for (size_type i = 0; i < newSize; i++)
+					tmp[i] = _begin[i];
+				_begin = tmp;
+				_end = tmp + newSize;
+				_capacity = newCapacity;
+			}
+	
+		protected:
+
+			T*			_begin;
+			T*			_end;
+			size_type	_capacity;
+			Allocator	_allocator;
+	
+	}; // class vector
 
 } // namespace ft
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,69 @@
 #include <iostream>
-#include "vector.hpp"
 
-using namespace ft;
+#ifdef STD
+	#include <vector>
+	using std::vector;
+#else
+	#include "vector.hpp"
+	using ft::vector;
+#endif
 
 #define PRINT(x) std::cout << x << std::endl;
 
 int	main() {
+
 	vector<int>	v;
 
+	//	Member types
 	{
-	vector<int>::iterator	begin;
-	vector<int>::iterator	end;
+		vector<int>::value_type				a; (void)a;
+		vector<int>::allocator_type			b; (void)b;
+		vector<int>::size_type				c; (void)c;
+		vector<int>::difference_type		d; (void)d;
+		vector<int>::reference				e = *v.begin(); (void)e;
+		vector<int>::const_reference		f = *v.begin(); (void)f;
+		vector<int>::pointer				g; (void)g;
+		vector<int>::const_pointer			h; (void)h;
+		vector<int>::iterator				i; (void)i;
+		vector<int>::const_iterator			j; (void)j;
+		vector<int>::reverse_iterator		k; (void)k;
+		vector<int>::const_reverse_iterator	l; (void)l;
+	}
 
-	begin = v.begin();
-	end = v.end();
-	PRINT(*(void **)&begin);
-	PRINT(*(void **)&end);
-	PRINT(v.capacity());
-	PRINT(v.size());
-	v.push_back(42);
+	{
+		vector<int>::iterator	begin;
+		vector<int>::iterator	end;
+
+		begin = v.begin();
+		end = v.end();
+		PRINT(*(void **)&begin);
+		PRINT(*(void **)&end);
+		PRINT(v.capacity());
+		PRINT(v.size());
+		v.push_back(42);
+		v.push_back(42);
+		v.push_back(42);
+		begin = v.begin();
+		end = v.end();
+		PRINT(*(void **)&begin);
+		PRINT(*(void **)&end);
+		PRINT(v.capacity());
+		PRINT(v.size());
+		v.pop_back();
+		v.pop_back();
+		v.pop_back();
+	}
+
+	//	Element access
+	{
+		PRINT(v.size());
+		v.push_back(42);
+		PRINT(v.front());
+		PRINT(v.back());
+		PRINT(v.at(0));
+		try { PRINT(v.at(1)); }
+			catch(const std::exception& e) { std::cerr << e.what() << '\n'; }
+		try { PRINT(v[-1]); }
+			catch(const std::exception& e) { std::cerr << e.what() << '\n'; }
 	}
 }
